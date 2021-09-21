45aa1a437305a6daf9acf19edee7300459d8f5b0
lalzat25 <luisalzatefdo@gmail.com>
2021-09-20 23:10:57 -0500

    Show date

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send("Hello World");
 });
 
+app.get('/date', (req, res)=>{
+    res.send(`Date is ${(new Date()).toLocaleDateString("es-CO")}`)
+});
+
 app.listen (port, ()=>{
     console.log(`Runing on localhost:${3000}`)
 });
